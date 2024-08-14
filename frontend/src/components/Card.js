6ba620926cbc8fb670c7de3fb3e9eb2099666935
Akash Kumar Tiwary <btech10172.22@bitmesra.ac.in>
2024-08-14 17:02:34 +0530

    Navbar Logic

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -8,14 +8,14 @@ const Card = (props) => {
   return (
     <div>
     <div className="card mt-3" style={{"width": "18rem", "maxHeight":"425px"}}>
-    <img className="card-img-top" style={{"width": "18rem", "minHeight":"200px"}} src={props.imgsrc} alt="Card image cap"/>
+    <img className="card-img-top" style={{"width": "18rem", "height":"200px", objectFit: "fill"}} src={props.imgsrc} alt="Card image cap"/>
       <div className="card-body">
         <h5 className="card-title">{props.foodName}</h5>
         <p className="card-text">{props.desc}</p>
         <div className='container w-100'>
             <select className='m-2 h-100 bg-success rounded'>
               {
-                Array.from(Array(6), (e,i) =>{
+                Array.from(Array(9), (e,i) =>{
                   return(
                     <option key={i+1} value={i+1}>{i+1}</option> 
                   )
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 const Navbar = () => {
+    const Navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem("authToken")
+        Navigate('/login')
+    }
   return (
     <div>
           <nav className="navbar navbar-expand-lg navbar-dark bg-success">
               <div className="container-fluid">
-                  <Link className="navbar-brand fs-1 fst-italic" to="/">
+                  <Link className="navbar-brand fs-1 fst-italic pb-2" to="/">
                       BiteBliss
                   </Link>
                   <button
@@ -20,25 +25,43 @@ const Navbar = () => {
                       <span className="navbar-toggler-icon"></span>
                   </button>
                   <div className="collapse navbar-collapse" id="navbarNav">
-                      <ul className="navbar-nav">
+                      <ul className="navbar-nav me-auto mb-2">
                           <li className="nav-item">
-                              <Link className="nav-link active" aria-current="page" to="/">
+                              <Link className="nav-link active fs-5" aria-current="page" to="/">
                                   Home
                               </Link>
                           </li>
-                          <li className="nav-item">
-                              <Link className="nav-link" to="/Login">
-                                  Login
-                              </Link>
-                          </li>
-                          <li className="nav-item">
-                              <Link className="nav-link" to="/createuser">
-                                  SignUp
-                              </Link>
-                          </li>
+                        {
+                        (localStorage.getItem("authToken")) ? 
+                        <li className="nav-item">
+                        <Link className="nav-link active fs-5" aria-current="page" to="/">
+                            My Orders
+                        </Link>
+                        </li> : ""
+                        }
                       </ul>
+                        {
+                            (localStorage.getItem("authToken")) ? 
+                            <div className='d-flex'>
+                            <div className="btn bg-white text-success">
+                                My Cart
+                            </div>
+                            <div className="btn bg-white text-danger mx-1" onClick={handleLogout}>
+                                Log-Out
+                            </div>
+                            </div> 
+                            :
+                            <div className='d-flex'>
+                            <Link className="btn bg-white text-success" to="/Login">
+                                Login
+                            </Link>
+                            <Link className="btn bg-white text-success mx-1" to="/createuser">
+                                SignUp
+                            </Link>
+                            </div>
+                        }                      
                   </div>
-              </div>
+                </div>
           </nav>
     </div>
   );
